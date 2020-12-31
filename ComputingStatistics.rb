019aba43d4e6a9f0f5ed7a45688eae4ecdbccc23
Connor Torrell <connortorrell@gmail.com>
2020-12-30 19:16:00 -0500

    added average functionality

--- ComputingStatistics.rb
+++ ComputingStatistics.rb
@@ -1,9 +1,29 @@
 class ComputingStatistics
-  def numbers(numbers)
+
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+  def numbers
     number_string = "Numbers: "
-    numbers.each do |number|
+    @numbers.each do |number|
       number_string += "#{number}, "
     end
     number_string[0...-2]
   end
+
+  def average
+    total = 0
+    @numbers.each do |number|
+      total += number
+    end
+    average = total.to_f / @numbers.length
+    average = average.to_s
+    if average.end_with?(".0")
+      "The average is #{average[0...-2]}"
+    else
+      "The average is #{average}"
+    end
+  end
+
 end
--- spec/ComputingStatistics_spec.rb
+++ spec/ComputingStatistics_spec.rb
@@ -1,10 +1,24 @@
 require './ComputingStatistics'
 
 describe "computing statistics" do
-  let(:computing_statistics) { ComputingStatistics.new }
-  context "returns entered numbers" do
-    it "correctly" do
-      expect(computing_statistics.numbers([100, 200, 300])).to eq("Numbers: 100, 200, 300")
+  let(:computing_statistics) { ComputingStatistics }
+  context "correctly returns entered numbers" do
+    it "with 1 number" do
+      expect(computing_statistics.new([100]).numbers).to eq("Numbers: 100")
+    end
+    it "with 2 numbers" do
+      expect(computing_statistics.new([100, 200]).numbers).to eq("Numbers: 100, 200")
+    end
+  end
+  context "correctly returns average number" do
+    it "with 1 number" do
+      expect(computing_statistics.new([100]).average).to eq("The average is 100")
+    end
+    it "with 2 numbers" do
+      expect(computing_statistics.new([100, 200]).average).to eq("The average is 150")
+    end
+    it "with odd numbers" do
+      expect(computing_statistics.new([107, 259, 323]).average).to eq("The average is 229.66666666666666")
     end
   end
 end
